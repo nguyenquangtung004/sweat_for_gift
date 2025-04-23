4077c7274b79485ab8bced34c49a0504c755d27b
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2025-04-23 16:30:57 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sweat_for_gift/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sweat_for_gift
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
+import 'package:flutter/cupertino.dart';
+
+Future<void> main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+  } catch (e) {
+    print(e); 
+  }
+}
