6df95b3138e4b40e42f4c239344017c67529d6fc
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2025-04-23 16:48:14 +0700

    feat: Đã bổ thư viện talker và khởi tạo app Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:sweat_for_gift/src/untils/log/talker.dart';
+import 'package:talker/talker.dart';
 
 Future<void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    
+    await Firebase.initializeApp();
+    talker.info('Đã khởi tạo Firebase thành công');
   } catch (e) {
-    print(e); 
+    talker.error('Đã khởi tạo Firebase thất bại:$e');
   }
+  
 }
---
+++ lib/src/untils/log/talker.dart
@@ -0,0 +1,8 @@
+import 'package:talker/talker.dart';
+
+final talker = Talker(
+  settings: TalkerSettings(
+    useConsoleLogs: true,
+    maxHistoryItems: 200
+  )
+);
